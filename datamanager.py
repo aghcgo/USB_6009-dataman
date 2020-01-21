e07d495ff2d660172badbb6e68523d38b03232d1
DiegoGarzaro <diego.garzaro@gmail.com>
2020-01-21 15:21:39 -0300

    Update datamanager.py

--- datamanager.py
+++ datamanager.py
@@ -114,12 +114,6 @@ class Dataman:
                              name='Buttom Signal'))
         fig2.show()
 
-    def plot_graph(self, data):
-        fig3 = go.Figure()
-        fig3.add_trace(go.Scatter(x=np.arange(len(data)), y=data,
-                            mode = 'lines', name='Data'))
-        fig3.show()
-
     def read_pmu_file(self, filename):
         '''
         File name without extension
@@ -128,27 +122,73 @@ class Dataman:
         with open(filename+extensao) as file:
             spam = csv.reader(file, delimiter=',')
             value = []
+            self.pmu_time_epoch = []
             self.pmu_time = []
             for j,row in enumerate(spam):
                 if j != 0:
                     value.append(float(row[1]))
-                    pmu_time = float(row[0])/1000
-                    pmu_time = datetime.datetime.fromtimestamp(pmu_time)
+                    pmu_aux = int(float(row[0])/1000)
+                    self.pmu_time_epoch.append(pmu_aux)
+                    pmu_time = datetime.datetime.fromtimestamp(pmu_aux)
                     self.pmu_time.append(pmu_time)
         return value
+    
+    def adjust_data(self, data, dots):
+        '''
+        Function to adapt 2 graphs (read_pmu_file('file'), points)
+        return 2D vector (time, values)
+        '''
+        
+        niusb_time = self.time1[0:8]
+        
+        pmu_time_string = []
+        for i in range(len(self.pmu_time)):
+            pmu_time_string.append(str(self.pmu_time[i])[11:-1] + str(self.pmu_time[i])[-1])
+        
+        #print(pmu_time_string)
+        #print(niusb_time)
+        
+        tam_rms = len(self.volt) / self.sample_rate
+        tam_data = self.pmu_time_epoch[-1] - self.pmu_time_epoch[0]
+        
+        time_initial_epoch = self.pmu_time_epoch[0] + ((tam_data - tam_rms) / 2)
+        
+        for i in range(len(data)):
+            if pmu_time_string[i] == niusb_time:
+                time_initial_epoch = self.pmu_time_epoch[i]
+        
+        time_initial = self.pmu_time_epoch[0] - time_initial_epoch
+        time_final = self.pmu_time_epoch[-1] - time_initial_epoch
+        vetor_x = np.arange(time_initial, time_final, dots)
+        out_data = [vetor_x, data]
+        #print(time_initial)
+        #print(time1)
+        #print(pmu_time1[0])
+        #print(pmu_time1[-1])
+        #print(time_final)
+        #print(vetor)
+        #print(len(vetor))
+        #print(len(pmu_volt))
+        return out_data
+        
+    def plot_graph(self, data):
+        fig3 = go.Figure()
+        fig3.add_trace(go.Scatter(x=np.arange(len(data)), y=data,
+                            mode = 'lines', name='Data'))
+        fig3.show()
 
     def plot_graph_rmsXcurrent(self, file_volt1, file_volt2, file_volt3, points_volt, file_curr1, file_curr2, file_curr3, points_curr):
         curr1_pmu = self.read_pmu_file(file_curr1)
         curr2_pmu = self.read_pmu_file(file_curr2)
         curr3_pmu = self.read_pmu_file(file_curr3)
+        
+        data_current1 = self.adjust_data(curr1_pmu, points_curr)
+        data_current2 = self.adjust_data(curr2_pmu, points_curr)
+        data_current3 = self.adjust_data(curr3_pmu, points_curr)
 
-        pmu_volt1 = self.adjust_data(self.volt_rms()[0], self.read_pmu_file(file_volt1), points_volt)
-        pmu_volt2 = self.adjust_data(self.volt_rms()[0], self.read_pmu_file(file_volt2), points_volt)
-        pmu_volt3 = self.adjust_data(self.volt_rms()[0], self.read_pmu_file(file_volt3), points_volt)
-
-        data_current1 = self.adjust_data(self.volt_rms()[0], curr1_pmu, points_curr)
-        data_current2 = self.adjust_data(self.volt_rms()[0], curr2_pmu, points_curr)
-        data_current3 = self.adjust_data(self.volt_rms()[0], curr3_pmu, points_curr)
+        pmu_volt1 = self.adjust_data(self.read_pmu_file(file_volt1), points_volt)
+        pmu_volt2 = self.adjust_data(self.read_pmu_file(file_volt2), points_volt)
+        pmu_volt3 = self.adjust_data(self.read_pmu_file(file_volt3), points_volt)
 
         tam_ax = len(self.volt_rms()[0])
 
@@ -160,27 +200,27 @@ class Dataman:
             secondary_y=False,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(tam_ax), y=pmu_volt1, name="Voltage PMU L1"),
+            go.Scatter(x=pmu_volt1[0], y=pmu_volt1[1], name="Voltage PMU L1"),
             secondary_y=False,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(tam_ax), y=pmu_volt2, name="Voltage PMU L2"),
+            go.Scatter(x=pmu_volt2[0], y=pmu_volt2[1], name="Voltage PMU L2"),
             secondary_y=False,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(tam_ax), y=pmu_volt3, name="Voltage PMU L3"),
+            go.Scatter(x=pmu_volt3[0], y=pmu_volt3[1], name="Voltage PMU L3"),
             secondary_y=False,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(tam_ax), y=data_current1, name="Current L1"),
+            go.Scatter(x=data_current1[0], y=data_current1[1], name="Current L1"),
             secondary_y=True,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(tam_ax), y=data_current2, name="Current L2"),
+            go.Scatter(x=data_current2[0], y=data_current2[1], name="Current L2"),
             secondary_y=True,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(tam_ax), y=data_current3, name="Current L3"),
+            go.Scatter(x=data_current3[0], y=data_current3[1], name="Current L3"),
             secondary_y=True,
         )
         # Add figure title
@@ -199,9 +239,9 @@ class Dataman:
         curr2_pmu = self.read_pmu_file(file_curr2)
         curr3_pmu = self.read_pmu_file(file_curr3)
 
-        data_current1 = self.adjust_data(self.volt_rms()[0], curr1_pmu, points)
-        data_current2 = self.adjust_data(self.volt_rms()[0], curr2_pmu, points)
-        data_current3 = self.adjust_data(self.volt_rms()[0], curr3_pmu, points)
+        data_current1 = self.adjust_data(curr1_pmu, points)
+        data_current2 = self.adjust_data(curr2_pmu, points)
+        data_current3 = self.adjust_data(curr3_pmu, points)
 
         tam_ax = len(self.volt_rms()[0])
 
@@ -213,15 +253,15 @@ class Dataman:
             secondary_y=False,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(tam_ax), y=data_current1, name="Current L1"),
+            go.Scatter(x=data_current1[0], y=data_current1[1], name="Current L1"),
             secondary_y=True,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(tam_ax), y=data_current2, name="Current L2"),
+            go.Scatter(x=data_current2[0], y=data_current2[1], name="Current L2"),
             secondary_y=True,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(tam_ax), y=data_current3, name="Current L3"),
+            go.Scatter(x=data_current3[0], y=data_current3[1], name="Current L3"),
             secondary_y=True,
         )
         # Add figure title
@@ -240,9 +280,9 @@ class Dataman:
         volt2_pmu = self.read_pmu_file(file_v2)
         volt3_pmu = self.read_pmu_file(file_v3)
 
-        data_volt1 = self.adjust_data(self.volt_rms()[0], volt1_pmu, points)
-        data_volt2 = self.adjust_data(self.volt_rms()[0], volt2_pmu, points)
-        data_volt3 = self.adjust_data(self.volt_rms()[0], volt3_pmu, points)
+        data_volt1 = self.adjust_data(volt1_pmu, points)
+        data_volt2 = self.adjust_data(volt2_pmu, points)
+        data_volt3 = self.adjust_data(volt3_pmu, points)
 
         tam_ax = len(self.volt_rms()[0])
 
@@ -254,15 +294,15 @@ class Dataman:
             secondary_y=False,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(tam_ax), y=data_volt1, name="Voltage L1"),
+            go.Scatter(x=data_volt1[0], y=data_volt1[1], name="Voltage L1"),
             secondary_y=True,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(tam_ax), y=data_volt2, name="Voltage L2"),
+            go.Scatter(x=data_volt2[0], y=data_volt2[1], name="Voltage L2"),
             secondary_y=True,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(tam_ax), y=data_volt3, name="Voltage L3"),
+            go.Scatter(x=data_volt3[0], y=data_volt3[1], name="Voltage L3"),
             secondary_y=True,
         )
         # Add figure title
@@ -277,7 +317,7 @@ class Dataman:
         fig.show()
         
     def plot_phase(self, file_pmu_curr, points):
-        curr_pmu = self.adjust_data(self.volt_rms()[0], self.read_pmu_file(file_pmu_curr), points)
+        curr_pmu = self.adjust_data(self.read_pmu_file(file_pmu_curr), points)
 
         tam_ax = len(self.volt_rms()[0])
 
@@ -293,7 +333,7 @@ class Dataman:
             secondary_y=False,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(tam_ax), y=curr_pmu, name="Current L2"),
+            go.Scatter(x=curr_pmu[0], y=curr_pmu[1], name="Current L2"),
             secondary_y=True,
         )
         # Add figure title
@@ -307,87 +347,20 @@ class Dataman:
         fig.update_yaxes(title_text="<b>Current Phase</b> Ampere [A]", secondary_y=True)
         fig.show()
 
-    def adjust_data(self, data_ref, data_adj, dots):
-        '''
-        Calc to adapt the 2 graphs
-        Calcula numero de pontos maximos para que seja possivel plotar os 2 graficos
-        com a mesma distribuicao no eixo x.
-
-        Equação da Reta
-        (y - yo) = m * (x - xo)
-        m = (y - yo) / (x - xo)
-
-        reta[posicao] = m * ( n(ponto da reta) * (y - yo) / res_points)
-
-        |---n---*---*---*---*---*---*---*---|
-        |---*---n---*---*---*---*---*---*---|
-        |---*---*---n---*---*---*---*---*---|
-        |---*---*---*---n---*---*---*---*---|
-        |---*---*---*---*---n---*---*---*---|
-        |---*---*---*---*---*---n---*---*---|
-        |---*---*---*---*---*---*---n---*---|
-        |---*---*---*---*---*---*---*---n---|
-        '''
-        tam_ref = len(data_ref)
-        tam_adj = len(data_adj)
-        
-
-        if (tam_ref - tam_adj) > (tam_ref*0.01):
-            #points = tam_ref
-            res_points = dots    # dots segundos -> 1 segundo
-            #res_points = points / tam_adj +1
-            #res_points = np.ceil(res_points)        # Arredonda para o teto
-            data_adjusted = []
-            for i in range(1, tam_adj):
-                y = data_adj[i]
-                yo = data_adj[i-1]
-                x = i
-                xo = i-1
-                m = (y - yo) / (x - xo)
-                for k in range(int(res_points)):
-                    data_adjusted.append(m * ((k+1) * (x - xo) / res_points) + yo)
-        else:
-            return data_adj
-
-        '''
-        if (tam_ref - tam_adj) > (tam_ref*0.01):
-            points = tam_ref
-            res_points = points / tam_adj
-            res_points = np.ceil(res_points)        # Arredonda para o teto
-            data_adjusted = []
-            for i in range(1, tam_adj):
-                y = data_adj[i]
-                yo = data_adj[i-1]
-                reta = np.arange(yo, y, res_points)
-                for k in range(len(reta)):
-                    data_adjusted.append(reta[k])
-        else:
-            return data_adj
-        '''
-        
-        return data_adjusted
-
-    def plot_2graphs(self, data1, data2, points):
+    def plot_2graphs(self, data1, points1, data2, points2):
         # Plot 2 graphs
-
-        if len(data1) > len(data2):
-            data_ref = data1
-            data_adj = data2
-        else:
-            data_ref = data2
-            data_adj = data1
-
-        data_adjusted = self.adjust_data(data_ref, data_adj, points)
+        data_adjusted1 = self.adjust_data(data1, points1)
+        data_adjusted2 = self.adjust_data(data2, points2)
 
         # Create figure with secondary y-axis
         fig = make_subplots(specs=[[{"secondary_y": True}]])
         # Add traces
         fig.add_trace(
-            go.Scatter(x=np.arange(len(data_ref)), y=data_ref, name="Data 1"),
+            go.Scatter(x=data1_adjusted1[0], y=data1_adjusted1[1], name="Data 1"),
             secondary_y=False,
         )
         fig.add_trace(
-            go.Scatter(x=np.arange(len(data_adjusted)), y=data_adjusted, name="Data 2"),
+            go.Scatter(x=data1_adjusted2[0], y=data1_adjusted2[1], name="Data 2"),
             secondary_y=True,
         )
         # Add figure title
